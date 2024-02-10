228a1205db018ff347b789bd55c6a8a5ef08b1e8
Piotr <trojanpiotrek@interia.pl>
2024-02-10 16:08:01 +0100

    Added Menu class and menu package

---
+++ src/main/java/org/onlineStorePackage/menu/Menu.java
@@ -0,0 +1,41 @@
+package org.onlineStorePackage.menu;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Menu {
+    private void printOption(){
+        System.out.println("Welcome in online store.");
+        System.out.println("Select an option: ");
+        System.out.println("1 - login.");
+        System.out.println("2 - register");
+        System.out.println("3 - exit");
+        System.out.println("Enter your choose: ");
+    }
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        int userChoise;
+        printOption();
+        try {
+            userChoise = scanner.nextInt();
+            switch (userChoise) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    System.out.println("Thank you, see you soon!");
+                    break;
+                default:
+                    System.out.println("Invalid choose. Please try again");
+                    run();
+                    break;
+            }
+        }
+            catch(InputMismatchException e){
+                System.out.println("You need to write a number! Please try again");
+                run();
+            }
+        scanner.close();
+    }
+}
